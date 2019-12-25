4c46822dcf78d675b28150776cc2905299bb6217
vincenterc <chung.menghsun@gmail.com>
2019-12-25 20:43:16 +0800

    Modify the description meta

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => (
   <>
     <Helmet>
       <title>p5.js starter</title>
-      <meta name="description" content="p5.js starter" />
+      <meta name="description" content="A starter for Gatsby.js with p5.js" />
       {/* <link rel="icon" href={favicon} /> */}
     </Helmet>
     {children}
